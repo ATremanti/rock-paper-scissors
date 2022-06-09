80c0a30f6c1853a84019bc3e39d8635474ebd9fe
ATremanti <arthurtremanti@gmail.com>
2022-06-08 22:17:55 -0300

    add css

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ rockB.addEventListener('click', () => {
     const results = document.querySelector('.displayRoundResults');
     results.textContent = playRound();
     checkWinner();
+    rockB.classList.add('pressed');
 });
 
 const paperB = document.querySelector('#paper');
@@ -17,6 +18,7 @@ paperB.addEventListener('click', () => {
     const results = document.querySelector('.displayRoundResults');
     results.textContent = playRound();
     checkWinner();
+    paperB.classList.add('pressed');
 });
 
 const scissorsB = document.querySelector('#scissors');
@@ -25,7 +27,18 @@ scissorsB.addEventListener('click', () => {
     const results = document.querySelector('.displayRoundResults');
     results.textContent = playRound();
     checkWinner();
+    scissorsB.classList.add('pressed');
 })
+
+const buttons = document.querySelectorAll('.rps');
+buttons.forEach(button => button.addEventListener('transitionend', removePressed));
+
+function removePressed(e) {
+    if(e.propertyName !== 'transform') 
+        return;
+        this.classList.remove('pressed');
+    
+}
 
 function computerPlays() {
     let x = Math.floor(Math.random() * 3) + 1
@@ -52,27 +65,27 @@ function playRound() {
     if (playerSelection == "rock" && computerSelection == "paper") {
         computerScore++
         cscore.textContent = `Computer score: ${computerScore}`;
-        return `The computer chose ${computerSelection}. You lose, paper beats rock :(`;
+        return `The computer chose ${computerSelection}. You lose, paper beats rock`;
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         yourScore++
         pscore.textContent = `Your score: ${yourScore}`;
-        return `The computer chose ${computerSelection}. You win! Rock beats scissors! :)`;
+        return `The computer chose ${computerSelection}. You win! Rock beats scissors!`;
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         yourScore++
         pscore.textContent = `Your score: ${yourScore}`;
-        return `The computer chose ${computerSelection}. You win! Paper beats rock! :)`;
+        return `The computer chose ${computerSelection}. You win! Paper beats rock!`;
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         computerScore++
         cscore.textContent = `Computer score: ${computerScore}`;
-        return `The computer chose ${computerSelection}. You lose, scissors beats paper :(`;
+        return `The computer chose ${computerSelection}. You lose, scissors beats paper`;
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         computerScore++
         cscore.textContent = `Computer score: ${computerScore}`;
-        return `The computer chose ${computerSelection}. You lose, rock beats scissors :(`;
+        return `The computer chose ${computerSelection}. You lose, rock beats scissors`;
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         yourScore++
         pscore.textContent = `Your score: ${yourScore}`;
-        return `The computer chose ${computerSelection}. You win! Scissors beats paper! :)`
+        return `The computer chose ${computerSelection}. You win! Scissors beats paper!`
     } else {
         return "Draw!"
     }
@@ -81,13 +94,11 @@ function playRound() {
 function checkWinner () {
     const gameOver = document.querySelector('.displayGameOver');
     if (yourScore === 5) {
-        gameOver.textContent = "Congratulations! You won!";
-        const buttons = document.querySelectorAll('.rps');
+        gameOver.textContent = "Congratulations! You won! Refresh the page to play again :)";
         buttons.forEach(button => button.disabled = true);
         
     } else if (computerScore === 5) {
-        gameOver.textContent = "You suck! The computer won!"
-        const buttons = document.querySelectorAll('.rps');
+        gameOver.textContent = "The computer won! Refresh the page to play again :("
         buttons.forEach(button => button.disabled = true);
     }
 }
